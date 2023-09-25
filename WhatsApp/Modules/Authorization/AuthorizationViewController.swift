f81d506d8b32d6a1b4ef52a9945ec092848e730d
Vladimir Fibe <motiw@icloud.com>
2023-09-25 10:32:23 +0600

    add BaseView

--- WhatsApp/Modules/Authorization/AuthorizationViewController.swift
+++ WhatsApp/Modules/Authorization/AuthorizationViewController.swift
@@ -3,8 +3,8 @@ import SnapKit
 
 class AuthorizationViewController: BaseViewController {
     private let titleLabel = {
-        $0.text = "Please confirm your country code and enter your phone number"
-        $0.font = .systemFont(ofSize: 16)
+        $0.text = "Login"
+        $0.font = .systemFont(ofSize: 35)
         $0.textAlignment = .center
         $0.numberOfLines = 0
         return $0
@@ -28,19 +28,13 @@ class AuthorizationViewController: BaseViewController {
 extension AuthorizationViewController {
     override func setupViews() {
         super.setupViews()
-        navigationItem.title = "Phone number"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .done,
-            target: self,
-            action: #selector(doneButtonTapped)
-        )
         [titleLabel, countryButton].forEach { view.addSubview($0)}
     }
 
     override func setupConstraints() {
         titleLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            $0.leading.trailing.equalToSuperview().inset(38)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            $0.leading.trailing.equalToSuperview().inset(16)
         }
 
         countryButton.snp.makeConstraints {
