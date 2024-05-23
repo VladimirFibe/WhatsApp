b740c96cebe88e90c47fbd634ecbb242138c6d12
Vladimir Fibe <motiw@icloud.com>
2024-05-23 20:49:36 +0500

    Setting text, background color and tail of the message

--- WhatsAppClone/Modules/Chat/MessagesDisplayDelegate+Chat.swift
+++ WhatsAppClone/Modules/Chat/MessagesDisplayDelegate+Chat.swift
@@ -1,5 +1,31 @@
 import MessageKit
+import UIKit
 
 extension ChatViewController: MessagesDisplayDelegate {
+    func textColor(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> UIColor {
+        .label
+    }
 
+    func backgroundColor(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> UIColor {
+        isFromCurrentSender(message: message) ? .chatOutgoingBubble : .chatIncomingBubble
+    }
+
+    func messageStyle(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> MessageStyle {
+        let tail: MessageStyle.TailCorner = isFromCurrentSender(message: message)
+        ? .bottomRight
+        : .bottomLeft
+        return .bubbleTail(tail, .curved)
+    }
 }
