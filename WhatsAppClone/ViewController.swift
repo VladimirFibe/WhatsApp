2061f321c4e536c752a778f090a8cf1cea4b81b9
Vladimir Fibe <motiw@icloud.com>
2024-08-19 21:11:26 +0500

    start

---
+++ WhatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by MacUser on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
