679ce83b81fb70641ef5af66dac6df8c93bb24b0
Vladimir Fibe <motiw@icloud.com>
2024-08-19 21:35:12 +0500

    add firebase

---
+++ WhatsAppClone/App/SceneDelegate.swift
@@ -0,0 +1,48 @@
+import UIKit
+import FirebaseAuth
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        start()
+    }
+    
+    func start() {
+        if Auth.auth().currentUser == nil {
+            print("login")
+            Auth.auth().signIn(withEmail: "motiw@icloud.com", password: "123456") { result, error in
+                if let error {
+                    print(error.localizedDescription)
+                }
+            }
+        } else {
+            try? Auth.auth().signOut()
+            print("logout")
+        }
+    }
+    
+    func setRootViewController(_ controller: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = controller
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = controller
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
+}
+
