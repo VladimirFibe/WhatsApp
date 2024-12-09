5f67d683e1affef6573a27dab28089668ac6bc78
Vladimir Fibe <motiw@me.com>
2024-12-09 05:12:18 +0500

    nfiguring Message View

--- WhatsAppClone/Modules/Chat/ChatViewController.swift
+++ WhatsAppClone/Modules/Chat/ChatViewController.swift
@@ -1,10 +1,13 @@
 import UIKit
 import MessageKit
+import InputBarAccessoryView
 import Realm
 
 final class ChatViewController: MessagesViewController {
     public let recent: Recent
-    
+    private let refreshControl = UIRefreshControl()
+    private let micButton = InputBarButtonItem()
+
     init(recent: Recent) {
         self.recent = recent
         super.init(nibName: nil, bundle: nil)
@@ -12,5 +15,48 @@ final class ChatViewController: MessagesViewController {
     
     @MainActor required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMessageCollectionView()
+        configureMessageInputBar()
+    }
+    
+    // MARK: - Configure
+    private func configureMessageCollectionView() {
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messageCellDelegate = self
+        messagesCollectionView.messagesLayoutDelegate = self
+
+        scrollsToLastItemOnKeyboardBeginsEditing = true
+        maintainPositionOnInputBarHeightChanged = true
+
+        messagesCollectionView.refreshControl = refreshControl
+    }
+    
+    private func configureMessageInputBar() {
+        messageInputBar.delegate = self
+        let attachButton = InputBarButtonItem()
+        attachButton.image = UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        attachButton.setSize(CGSize(width: 30, height: 30), animated: false)
+        attachButton.onTouchUpInside {[weak self] _ in self?.actionAttachMessage()}
+
+        micButton.image = UIImage(systemName: "mic.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        micButton.setSize(CGSize(width: 30, height: 30), animated: false)
+
+        messageInputBar.setStackViewItems([attachButton], forStack: .left, animated: false)
+        messageInputBar.setLeftStackViewWidthConstant(to: 36, animated: false)
+        messageInputBar.inputTextView.isImagePasteEnabled = false
+        messageInputBar.backgroundView.backgroundColor = .systemBackground
+        messageInputBar.inputTextView.backgroundColor = .systemBackground
+
+//        updateMicButtonStatus(show: true)
+        // add gesutre recognizer
+    }
+    
+    private func actionAttachMessage() {
+        print(#function)
     }
 }
