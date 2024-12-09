455fc656e60eeb966874fdcda8b3823066ca9b60
Vladimir Fibe <motiw@me.com>
2024-12-09 05:28:22 +0500

    Implementing chat extensions

---
+++ WhatsAppClone/Modules/Chat/MessageCellDelegate+Chat.swift
@@ -0,0 +1,12 @@
+//
+//  MessageCellDelegate+Chat.swift
+//  WhatsAppClone
+//
+//  Created by Vladimir Fibe on 09.12.2024.
+//
+
+import MessageKit
+
+extension ChatViewController: MessageCellDelegate {
+    
+}
---
+++ WhatsAppClone/Modules/Chat/MessagesDataSource+Chat.swift
@@ -0,0 +1,18 @@
+import UIKit
+import MessageKit
+
+extension ChatViewController: MessagesDataSource {
+    var currentSender: any MessageKit.SenderType {
+        <#code#>
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> any MessageKit.MessageType {
+        <#code#>
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        <#code#>
+    }
+    
+    
+}
